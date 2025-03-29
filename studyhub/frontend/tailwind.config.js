47ad1e77439c3fa9736c74e0ca6667b3fefa95df
Indranil Patra <simplyrin2@gmail.com>
2025-03-29 13:11:44 +0530

    Fixed response formatting

--- studyhub/frontend/tailwind.config.js
+++ studyhub/frontend/tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,10 +5,36 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        'code': ['"Source Code Pro"', 'monospace'],
+      },
       typography: {
         DEFAULT: {
           css: {
             maxWidth: 'none',
+            pre: {
+              backgroundColor: '#eff0f1',
+              color: '#073763',
+              overflowX: 'auto',
+              whiteSpace: 'pre-wrap',
+              wordBreak: 'break-word',
+              padding: '0.5rem',
+              fontFamily: '"Source Code Pro", monospace',
+              fontSize: '1rem',
+              fontWeight: '500',
+              width: '100%',
+            },
+            'pre code': {
+              backgroundColor: 'transparent',
+              borderWidth: '0',
+              borderRadius: '0',
+              padding: '0',
+              color: 'inherit',
+              fontSize: '0.875rem',
+              fontFamily: 'inherit',
+              lineHeight: '1.7142857',
+              whiteSpace: 'pre-wrap',
+            },
             code: {
               backgroundColor: '#f3f4f6',
               padding: '0.2em 0.4em',
@@ -22,6 +47,28 @@ module.exports = {
             'code::after': {
               content: '""'
             }
+          }
+        },
+        // Add dark mode styles
+        invert: {
+          css: {
+            code: {
+              backgroundColor: '#282b2d',
+              color: '#e2e8f0',
+            },
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            },
+            pre: {
+              backgroundColor: '#282b2d',
+              color: '#cfe2f3',
+            },
+            'pre code': {
+              color: '#cfe2f3',
+            },
           }
         }
       }
